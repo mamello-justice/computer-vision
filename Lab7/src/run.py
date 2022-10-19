3f2151505bd881f5f05b2fc90df60a9cd12a0247
Mamello Seboholi <j.seboholi@gmail.com>
2022-10-19 20:43:44 +0200

    Lab 7 - Only run when name is main

--- Lab7/src/run.py
+++ Lab7/src/run.py
@@ -1,12 +1,13 @@
 from classes import *
 
-puzzle = Puzzle(MATCH_IMGS)
-corner_piece = puzzle.pieces[3]
+if __name__ == '__main__':
+    puzzle = Puzzle(MATCH_IMGS)
+    corner_piece = puzzle.pieces[3]
 
-# Start BFS by adding in the bottom left corner piece
-queue = []
-queue.append(corner_piece)
-corner_piece.insert()
-corner_piece.inserted = True
+    # Start BFS by adding in the bottom left corner piece
+    queue = []
+    queue.append(corner_piece)
+    corner_piece.insert()
+    corner_piece.inserted = True
 
-# TODO: Rest of BFS
+    # TODO: Rest of BFS
