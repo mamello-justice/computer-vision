3bec90ce531b9c98f6de8c6772131382a963460a
Mamello Seboholi <j.seboholi@gmail.com>
2022-11-02 19:16:09 +0200

    Move assignment to own module

--- Assignment.bk/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(
-    name='cv_gmm_deep',
-    version='0.0.1',
-    description='Private package for GMM and Deep Learning utilities',
-    url='git@github.com:mamello-justice/cv-gmm-deep.git'
-)
